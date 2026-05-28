944e4ebf8ea86ddca49894c0635cfa458832d072
emin-geze <m.emin.geze@gmail.com>
2026-05-28 08:57:43 +0300

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    minWidth: 800,
+    minHeight: 600,
+    icon: path.join(__dirname, 'icon.ico'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      webviewTag: true // Web sayfalarını güvenli kutuda açmak için şart
+    }
+  });
+
+  mainWindow.loadFile('index.html');
+  // İstersen geliştirici araçlarını açabilirsin:
+  // mainWindow.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
